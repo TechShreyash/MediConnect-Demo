0b0aba6b137499470c7261886aef0ea4bd61b8ef
TechShreyash <techshreyash123@gmail.com>
2024-10-24 23:32:44 +0530

    Update database.py

--- Backend/utils/database.py
+++ Backend/utils/database.py
@@ -37,7 +37,7 @@ async def check_auth(email: str, password: str) -> bool:
         return False, "Invalid Email"
 
 
-# Shop Functions
+# Shop Details Functions
 
 # Shop Data {"email": str, "shop_name": str, "shop_address": str, "shop_phone": str}
 
@@ -49,3 +49,30 @@ async def update_shop(email, data):
 async def get_shop(email: str):
     shop_data = await SHOPDB.find_one({"email": email})
     return True, shop_data
+
+
+# Medicine Functions
+
+# Medicine Data {"name": str, "brand": str, "price": int, "quantity": int}
+
+
+async def add_medicine(email: str, data: dict):
+    await SHOPDB.update_one(
+        {"email": email}, {"$push": {"medicines": data}}, upsert=True
+    )
+    return True, "Medicine Added Successfully"
+
+
+async def update_medicine(email: str, data: dict):
+    await SHOPDB.update_one(
+        {"email": email, "medicines.name": data["name"]},
+        {"$set": {"medicines.$": data}},
+    )
+    return True, "Medicine Updated Successfully"
+
+
+async def get_medicines(email: str):
+    shop_data = await SHOPDB.find_one({"email": email})
+    if shop_data:
+        return shop_data["medicines"]
+    return []
